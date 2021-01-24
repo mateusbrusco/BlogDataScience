ba468ac003ac96328c9b7d09ccf1cdfd08582f00
mateusbrusco <mateusbrusco@gmail.com>
2021-01-24 18:50:11 -0300

    Ajustes no Service Worker para remoção de cache antigo.

--- public/serviceWorker.js
+++ public/serviceWorker.js
@@ -1,8 +1,10 @@
-
+var cacheEstaticoAtual = 'static-v2';
+var cacheDinamicoAtual = 'dynamic';
+//comentario
 self.addEventListener('install', function(event) {
   console.log('[Service Worker] Installing Service Worker ...', event);
   event.waitUntil(
-    caches.open('static')
+    caches.open(cacheEstaticoAtual)
       .then(function(cache) {
         console.log('[Service Worker] Precaching App Shell...');
         return cache.addAll([
@@ -27,6 +29,17 @@ self.addEventListener('install', function(event) {
 
 self.addEventListener('activate', function(event) {
   console.log('[Service Worker] Activating Service Worker ....', event);
+  event.waitUntil(
+    caches.keys()
+      .then(function(keylist) {
+        return Promise.all(keylist.map(function(key) {
+          if (key !== cacheEstaticoAtual && key !== cacheDinamicoAtual) {
+            console.log('[Service Worker] Removing old cache...', key);
+            return caches.delete(key);
+          }
+        }));
+      })
+  );
   return self.clients.claim();
 });
 
@@ -37,7 +50,17 @@ self.addEventListener('fetch', function(event) {
         if (response) {
           return response;
         } else {
-          return fetch(event.request);
+          return fetch(event.request)
+          .then(function(res) {
+            return caches.open(cacheDinamicoAtual)
+            .then(function(cache) {
+              cache.put(event.request.url, res.clone());
+              return res;
+            })
+          })
+          .catch(function(err) {
+
+          })
         }
       })
   );
