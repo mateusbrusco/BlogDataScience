21652eb813661cfa9af0e95c7e8c467be1cce6ff
mateusbrusco <mateusbrusco@gmail.com>
2021-01-23 21:36:24 -0300

    Ajustes no Service Worker para cache de Shell.

--- static/sw.js
+++ static/sw.js
@@ -1,5 +1,27 @@
 self.addEventListener('install', function(event) {
     console.log('[Service Worker] Instalando o Service Worker...', event);
+    event.waitUntil(
+        caches.open('Static')
+            .then(function(cache) {
+                console.log('[Service Worker] Precaching app shell');
+                cache.addAll([
+                    '/',
+                    'index.html',
+                    '/about/index.html',
+                    '/css/font.css',
+                    '/css/main.css',
+                    '/js/math-code.js'
+                    '/fonts/lato-v11-latin-regular.woff',
+                    '/fonts/lato-v11-latin-regular.woff2',
+                    '/fonts/merriweather-v13-latin-regular.woff',
+                    '/fonts/merriweather-v13-latin-regular.woff2',
+                    'favicon.ico',
+                    '/images/logo_pfc.png',
+                    'images/hugo-log.png',
+                    '/about/'
+                ]);
+            })
+        )
 });
 
 self.addEventListener('activate', function(event) {
@@ -8,6 +30,14 @@ self.addEventListener('activate', function(event) {
 });
 
 self.addEventListener('fetch', function(event) {
-    console.log('[Service Worker] Fetching something...', event);
-    event.respondWith();
+    event.respondWith(
+        caches.match(event, request)
+            .then(function(response) {
+                if(response) {
+                    return response;
+                } else {
+                    return fetch(event, request);
+                }
+            })
+    );
 });
